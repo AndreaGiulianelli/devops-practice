a9dfe29ab7bdaf371a716c07e8ca2c133766af08
Andrea Giulianelli <andrygiulio@gmail.com>
2022-12-18 17:20:21 +0100

    build: added local versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    id("org.danilopianini.git-sensitive-semantic-versioning-gradle-plugin") version "0.3.20"
     // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
