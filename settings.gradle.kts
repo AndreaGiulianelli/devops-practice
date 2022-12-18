d8644d2e766a9c5a7112bfc40536a6f4f1339dd1
Andrea Giulianelli <andrygiulio@gmail.com>
2022-12-18 19:13:52 +0100

    chore: change name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.23"
 }
-rootProject.name = "devops-practice"
+rootProject.name = "devops.practice"
 
 gitHooks {
     commitMsg {
