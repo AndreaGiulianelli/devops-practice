7237e10074f3ca623410b3f99f361fc2633dc87c
Andrea Giulianelli <andrygiulio@gmail.com>
2022-12-19 18:21:13 +0100

    fix: fix username in greetings

--- src/main/kotlin/devops/App.kt
+++ src/main/kotlin/devops/App.kt
@@ -3,5 +3,5 @@ package devops
 fun main(args: Array<String>) {
     val name = "Andrea"
     val surname = "Giulianelli"
-    println("Hello World from ${name.lowercase()}.${surname.lowercase()}!")
+    println("Hello World from ${name}${surname}!")
 }
